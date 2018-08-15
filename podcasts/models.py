fb6175e8de63b9fa4a461314c5fe233619327b56
cyanidesayonara <cyanidesayonara@gmail.com>
2018-08-15 13:48:31 +0300

    last-played fix & css fix

--- podcasts/models.py
+++ podcasts/models.py
@@ -1167,7 +1167,7 @@ class Episode(models.Model):
         results = {
             "episodes": episodes,
             "header": "Last played episodes",
-            "view": "last_played",
+            "view": "last-played",
         }
         cache.set("last_played", results, 60 * 60 * 24)
     
